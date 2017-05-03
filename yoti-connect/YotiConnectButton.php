601e4bfc9b4c95c41e8cbded84d42d326401aef2
Simon Tong <simon@supplyware.co.uk>
2017-05-03 14:28:17 +0100

    add yoti button

--- yoti-connect/YotiConnectButton.php
+++ yoti-connect/YotiConnectButton.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class YotiConnectButton
  *
@@ -15,64 +16,101 @@ class YotiConnectButton
         global $wpdb;
 
         $testToken = null;
-        if (YotiConnectHelper::mockRequests())
-        {
+        if (YotiConnectHelper::mockRequests()) {
             $testToken = file_get_contents(__DIR__ . '/sdk/sample-data/connect-token.txt');
         }
 
         // no config? no button
         $config = YotiConnectHelper::getConfig();
-        if (!$config && !$testToken)
-        {
+        if (!$config && !$testToken) {
             return null;
         }
 
-        if (!is_user_logged_in())
-        {
-            if (YotiConnectHelper::mockRequests())
-            {
-                $url = site_url('wp-login.php') . '?yoti-connect=1&action=link&token=' . $testToken . ($redirect ? '&redirect=' . rawurlencode($redirect) : '');
-            }
-            else
-            {
-                $url = YotiConnectHelper::getLoginUrl();
-            }
-            $label = 'Sign on with Yoti';
+        $linkButton = '<span
+            data-yoti-application-id="' . $config['yoti_app_id'] . '"
+            data-yoti-type="inline"
+            data-yoti-scenario-id="c8e2543d-1685-4e01-b2c6-024d6b527ce3"
+            data-size="small">
+            %s
+        </span>
+        <script>
+            _ybg.config.service = "https://www.yoti.com/connect/";
+            _ybg.init();
+        </script>';
+
+        if (!is_user_logged_in()) {
+            $button = sprintf($linkButton, 'Log in with Yoti');
         }
-        else
-        {
+        else {
             $currentUser = wp_get_current_user();
             $yotiId = get_user_meta($currentUser->ID, 'yoti_connect.identifier');
-            if (!$yotiId)
-            {
-                if (YotiConnectHelper::mockRequests())
-                {
-                    $url = site_url('wp-login.php') . '?yoti-connect=1&action=link&token=' . $testToken . ($redirect ? '&redirect=' . rawurlencode($redirect) : '');
-                }
-                else
-                {
-                    $url = YotiConnectHelper::getLoginUrl();
-                }
-                $label = 'Link account to Yoti';
+            if (!$yotiId) {
+                $button = sprintf($linkButton, 'Link account with Yoti');
             }
-            else
-            {
+            else {
                 $url = site_url('wp-login.php') . '?yoti-connect=1&action=unlink&redirect=' . ($redirect ? '&redirect=' . rawurlencode($redirect) : '');
                 $label = 'Unlink account from Yoti';
+                $button = '<a class="yoti-connect-button" href="' . $url . '">' . $label . '</a>';
             }
         }
 
         $message = YotiConnectHelper::getFlash();
-        $html = '<div class="yoti-connect">';
-        if ($message)
-        {
+        $html = '<div class="yoti-connect"> ';
+        if ($message) {
             $html .= '<div class="' . ($message['type'] == 'error' ? 'error' : 'message') . ' notice">' .
                 '<p><strong>' . $message['message'] . '</strong></p>' .
                 '</div>';
         }
-        $html .= '<a class="yoti-connect-button" href="' . $url . '">' . htmlspecialchars($label) . '</a>';
+        $html .= $button;
         $html .= '</div>';
 
         return $html;
+
+//        if (!is_user_logged_in())
+//        {
+//            if (YotiConnectHelper::mockRequests())
+//            {
+//                $url = site_url('wp - login . php') . ' ? yoti - connect = 1 & action = link & token = ' . $testToken . ($redirect ? ' & redirect = ' . rawurlencode($redirect) : '');
+//            }
+//            else
+//            {
+//                $url = YotiConnectHelper::getLoginUrl();
+//            }
+//            $label = 'Sign on with Yoti';
+//        }
+//        else
+//        {
+//            $currentUser = wp_get_current_user();
+//            $yotiId = get_user_meta($currentUser->ID, 'yoti_connect . identifier');
+//            if (!$yotiId)
+//            {
+//                if (YotiConnectHelper::mockRequests())
+//                {
+//                    $url = site_url('wp - login . php') . ' ? yoti - connect = 1 & action = link & token = ' . $testToken . ($redirect ? ' & redirect = ' . rawurlencode($redirect) : '');
+//                }
+//                else
+//                {
+//                    $url = YotiConnectHelper::getLoginUrl();
+//                }
+//                $label = 'Link account to Yoti';
+//            }
+//            else
+//            {
+//                $url = site_url('wp - login . php') . ' ? yoti - connect = 1 & action = unlink & redirect = ' . ($redirect ? ' & redirect = ' . rawurlencode($redirect) : '');
+//                $label = 'Unlink account from Yoti';
+//            }
+//        }
+//
+//        $message = YotiConnectHelper::getFlash();
+//        $html = ' <div class="yoti-connect"> ';
+//        if ($message) {
+//            $html .= '<div class="' . ($message['type'] == 'error' ? 'error' : 'message') . ' notice"> ' .
+//                '<p><strong> ' . $message['message'] . ' </strong></p> ' .
+//                '</div> ';
+//        }
+//        $html .= '<a class="yoti-connect-button" href = "' . $url . '"> ' . htmlspecialchars($label) . '</a> ';
+//        $html .= '</div> ';
+//
+//        return $html;
     }
 }
