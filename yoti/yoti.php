23ddd0fb96a904f10399d2eb08e1597d789b3f84
Moussa Sidibe <moussa.sidibe@yoti.com>
2018-07-31 16:13:18 +0100

    SDK-458: Update WP landing page summary and banner

--- yoti/yoti.php
+++ yoti/yoti.php
@@ -4,7 +4,7 @@
 Plugin Name: Yoti
 Plugin URI: https://wordpress.org/plugins/yoti/
 Description: Let Yoti users quickly register on your site.
-Version: 1.1.8
+Version: 1.1.9
 Author: Yoti SDK.
 Author URI: https://yoti.com
 */
