9094c21c64cbccb93a3277f9f8cb8759f97e56e0
David Grayston <davidgrayston@googlemail.com>
2019-03-27 13:59:06 +0000

    SDK-885: Update tag for release

--- yoti/yoti.php
+++ yoti/yoti.php
@@ -4,7 +4,7 @@
 Plugin Name: Yoti
 Plugin URI: https://wordpress.org/plugins/yoti/
 Description: Let Yoti users quickly register on your site.
-Version: 1.3.0
+Version: 1.3.1
 Author: Yoti SDK.
 Author URI: https://yoti.com
 */
