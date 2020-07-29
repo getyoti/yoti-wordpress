c3594bb49d0475963aafae28bd36f7978197be7d
David Grayston <davidgrayston@googlemail.com>
2020-07-29 23:54:53 +0100

    SDK-1689: Return early when aliasing legacy classes

--- yoti/autoload.php
+++ yoti/autoload.php
@@ -19,6 +19,7 @@ spl_autoload_register(function ($class) {
             E_USER_DEPRECATED
         );
         class_alias($deprecated[$class], $class);
+        return;
     }
 
     $prefix = 'Yoti\\WP\\';
