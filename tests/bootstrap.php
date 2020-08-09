1176cdbb4d4a47690af833d9cb6ec0780441c8ff
David Grayston <davidgrayston@googlemail.com>
2020-08-09 01:49:39 +0100

    SDK-1689: Use composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,22 +30,3 @@ tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
 // Start up the WP testing environment.
 require $tests_dir . '/includes/bootstrap.php';
-
-spl_autoload_register(function ($class) {
-    $prefix = 'Yoti\\WP\\Test\\';
-
-    $base_dir = __DIR__ . '/';
-
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
-
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file)) {
-        require $file;
-    }
-});
