d007a49a69765ea49823a4198a55a8b5c0ad2c1c
David Grayston <davidgrayston@googlemail.com>
2020-08-10 14:48:11 +0100

    SDK-1689: Tidy up Docker demo

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -4,7 +4,7 @@ docker-compose up -d wordpress-dev
 PLUGIN_PATH="/var/www/html/wp-content/plugins/yoti"
 
 # Install WordPress testing environment.
-docker-compose exec -w $PLUGIN_PATH wordpress-dev sh -c './bin/install-wp-tests.sh wordpress-test root root wordpress-db $WORDPRESS_VERSION'
+docker-compose exec -w $PLUGIN_PATH wordpress-dev sh -c './bin/install-wp-tests.sh wordpress-test root $MYSQL_ROOT_PASSWORD wordpress-db $WORDPRESS_VERSION' >/dev/null 2>&1
 
 # Coding Standards
 docker-compose exec -w $PLUGIN_PATH wordpress-dev ./vendor/bin/phpcs
